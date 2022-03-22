4c904df8cd30749d22d19c75e990ab3d4c89de10
saifulalamfahime <you@example.com>
2022-03-22 20:06:37 +0600

    backend

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+module.exports = {
+    products: [
+        {
+            _id: '1',
+            name: 'Lorem ipsum dolor sit amet.',
+            catagory: 'lorem',
+            image: '/images/product-1.jpg',
+            price: 14,
+            brand: 'ipsumLM',
+            rating: 4.5,
+            numReviws:10,
+            countInStock: 6
+        },
+        {
+            _id: '2',
+            name: 'Lorem ipsum dolor sit amet.',
+            catagory: 'lorem',
+            image: '/images/product-2.jpg',
+            price: 134,
+            brand: 'ipsumLM',
+            rating: 4.5,
+            numReviws:10,
+            countInStock: 6
+        },
+        {
+            _id: '3',
+            name: 'Lorem ipsum dolor sit amet.',
+            catagory: 'lorem',
+            image: '/images/product-3.jpg',
+            price: 4,
+            brand: 'ipsumLM',
+            rating: 4.5,
+            numReviws:10,
+            countInStock: 6
+        },
+        {
+            _id: '4',
+            name: 'Lorem ipsum dolor sit amet.',
+            catagory: 'lorem',
+            image: '/images/product-4.jpg',
+            price: 14,
+            brand: 'ipsumLM',
+            rating: 4.5,
+            numReviws:10,
+            countInStock: 6
+        },
+        {
+            _id: '5',
+            name: 'Lorem ipsum dolor sit amet.',
+            catagory: 'lorem',
+            image: '/images/product-5.jpg',
+            price: 3,
+            brand: 'ipsumLM',
+            rating: 4.5,
+            numReviws:10,
+            countInStock: 6
+        },
+        {
+            _id: '6',
+            name: 'Lorem ipsum dolor sit amet.',
+            catagory: 'lorem',
+            image: '/images/product-6.jpg',
+            price: 8,
+            brand: 'ipsumLM',
+            rating: 4.5,
+            numReviws:10,
+            countInStock: 6
+        },
+    ]
+}
