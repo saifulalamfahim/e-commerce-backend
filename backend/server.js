c2fcf80ec51853c0ff2501f4acb157ab5e3e1b33
saifulalamfahime <you@example.com>
2022-03-23 16:25:21 +0600

    transfer data from backend to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const cors = require('cors');
 const data = require('./data.js');
 
-const app = express();
 
+const app = express();
+app.use(cors());
 app.get("/api/products", (req, res) =>{
     res.send(data.products);
 });
 
 app.listen(5000, () => {
-    console.log('server at http://localhost:5000');
     console.log('server at http://localhost:5000');
 })
