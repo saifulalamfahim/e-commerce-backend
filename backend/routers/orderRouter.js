5eba777f87787f2496e3e810bdc230715a80a208
saifulalamfahime <you@example.com>
2022-05-19 15:52:36 +0600

    order screen

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -5,6 +5,19 @@ import { Router } from 'express';
 
 const orderRouter = Router();
 
+orderRouter.get (
+    '/:id',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            res.send(order);
+        } else {
+            res.status(404).send({ message: 'Order Not Found'});
+        }
+    })
+);
+
 orderRouter.post (
     '/',
     isAuth,
